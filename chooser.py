c6e10b258e536f03cf5014f7e945962def48fc01
Cole Sabin <csabin@uoregon.edu>
2019-08-01 16:41:07 -0700

    Made a random button

--- chooser.py
+++ chooser.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+from random import randrange
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "<h1> Good Ham </h1>"
+
+@app.route("/random")
+def random():
+    return render_template('random.html')
+
+@app.route("/rand_num")
+def rand_num():
+    result = {"number": randrange(1000000000)}
+    print(result)
+    return jsonify(result=result)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
